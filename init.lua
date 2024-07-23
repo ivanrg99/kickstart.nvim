33d5b0bf228fde36b1452014bd69a71ac62534c4
Ivan <ivan@infinitefieldtrading.com>
2024-07-23 11:29:06 +0200

    fix

--- init.lua
+++ init.lua
@@ -286,19 +286,15 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
     end,
   },
 
@@ -570,7 +566,7 @@ require('lazy').setup({
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-      capabilities.textDocument.completion.completionItem.snippetSupport = false;
+      capabilities.textDocument.completion.completionItem.snippetSupport = false
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
@@ -695,17 +691,17 @@ require('lazy').setup({
       --     end
       --     return 'make install_jsregexp'
       --   end)(),
-        -- dependencies = {
-        --   `friendly-snippets` contains a variety of premade snippets.
-        --      See the README about individual language/framework/plugin snippets:
-        --      https://github.com/rafamadriz/friendly-snippets
-        --   {
-        --     'rafamadriz/friendly-snippets',
-        --     config = function()
-        --       require('luasnip.loaders.from_vscode').lazy_load()
-        --     end,
-        --   },
-        -- },
+      -- dependencies = {
+      --   `friendly-snippets` contains a variety of premade snippets.
+      --      See the README about individual language/framework/plugin snippets:
+      --      https://github.com/rafamadriz/friendly-snippets
+      --   {
+      --     'rafamadriz/friendly-snippets',
+      --     config = function()
+      --       require('luasnip.loaders.from_vscode').lazy_load()
+      --     end,
+      --   },
+      -- },
       -- },
       -- 'saadparwaiz1/cmp_luasnip',
 
@@ -1008,5 +1004,8 @@ require('gruvbox').setup {
   dim_inactive = false,
   transparent_mode = false,
 }
-vim.cmd 'colorscheme github_dark_high_contrast'
+-- vim.cmd 'colorscheme github_dark_high_contrast'
+vim.cmd 'colorscheme lunaperche'
+-- vim.cmd 'colorscheme vim'
+-- vim.opt.termguicolors = false
 -- vim.o.background = 'dark' -- or 'light'
